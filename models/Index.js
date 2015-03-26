646f00de6c8930a4db653d4328c6529320e6bb8a
Jordan <Jsmilovic@gmail.com>
2015-03-26 17:56:36 -0400

    initial commit

---
+++ models/Index.js
@@ -0,0 +1,9 @@
+var normalizedPath = require("path").join(__dirname, "");
+var modules = {};
+
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+    if (file != "Index.js" && file.indexOf('_Schema') <= 0) {
+        modules[file.split('.')[0]] = require('./' + file);
+    }
+});
+module.exports = modules;
