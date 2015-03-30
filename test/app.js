dce167842995dd6c3322aaf2348a28fc4a90efb7
Will Brand <will.brand@gmail.com>
2015-03-30 16:02:59 -0400

    Stub for checking validity of schema

--- test/app.js
+++ test/app.js
@@ -13,6 +13,18 @@ describe('tools', function () {
     tools.createModel('SimpleTest', testOpts.schemas.simple, done);
   });
 
+  it('should throw an error on being given an invalid schema', function(done) {
+    try {
+      tools.createModel('SimpleTest', testOpts.schemas.invalid, done);
+    } catch(e) {
+      done();
+    }
+  });
+
+  it('should be able to create a model with a very long name', function(done) {
+    tools.createModel('LongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTestLongNameTest', testOpts.schemas.simple, done);
+  });
+
   it('should be able to create a model using each of the Mongoose schema types', function(done) {
     tools.createModel('ComplexTest', testOpts.schemas.complex, done);
   });
--- test/init.js
+++ test/init.js
@@ -1,27 +1,30 @@
 assert = require("assert");
 should = require('should');
 mongoose = require('mongoose');
-Schema = mongoose.Schema;
 
 Tools = require(__dirname + '/../index.js');
 tools = new Tools({modelsDir: __dirname + '/data/models/', modelTemplatesDir: __dirname + '/../views/'});
 
 testOpts = {
   schemas: {
-    simple: "{                                      \
-      name: String                                  \
+    simple: "{ \
+      name: String \
     }",
-    complex: "{                                     \
-      title:  String,                               \
-      author: String,                               \
-      body:   String,                               \
-      comments: [{ body: String, date: Date }],     \
-      date: { type: Date, default: Date.now },      \
-      hidden: Boolean,                              \
-      meta: {                                       \
-        votes: Number,                              \
-        favs:  Number                               \
-      }                                             \
+    complex: "{ \
+      title:  String, \
+      author: String, \
+      body:   String, \
+      comments: [{ body: String, date: Date }], \
+      date: { type: Date, default: Date.now }, \
+      hidden: Boolean, \
+      meta: { \
+        votes: Number, \
+        favs:  Number \
+      } \
+    }",
+    invalid:  "{ \
+      title:  'String', \
+      author: Strings \
     }"
   }
 };
